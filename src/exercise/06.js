52a5c3846c0203b6ec3afbdbbec534f2b3576411
Luci Temple <luci.temple@annalise.ai>
2022-08-04 16:26:45 +1000

    Exercise 6.8 use resetKeys

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -81,12 +81,20 @@ function App() {
     setPokemonName(newPokemonName)
   }
 
+  function handleReset() {
+    setPokemonName('')
+  }
+
   return (
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={handleReset}
+          resetKeys={[pokemonName]}
+        >
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
